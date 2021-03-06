9b7a8269d5f9421f0e2c42a1983bd1deb40b6b91
Nick Monsma <nmonsma@outlook.com>
2021-03-05 17:20:08 -0800

    feat: Added to the js file. In the middle of setting the code to add "active" to the active section.

--- js/app.js
+++ js/app.js
@@ -18,12 +18,23 @@
  * 
 */
 
+const sectionsArray = document.getElementsByTagName('section');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+
+function elementIsNearTop(element) {
+    const y = element.getBoundingClientRect()['y'];
+    if (y < 200 && y > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 
 
@@ -35,8 +46,33 @@
 
 // build the nav
 
+for (const section of sectionsArray) {
+    const id = section.id;
+    const name = section.getElementsByTagName('h2')[0].innerText;
+
+    const navbarElement = document.createElement('li');
+    navbarElement.innerHTML = `<a href="#${id}">${name}</a>`;
+    document.getElementById('navbar__list').appendChild(navbarElement);
+}
+
 
 // Add class 'active' to section when near top of viewport
+
+
+// TODO: Change this function so that "active-section" is a class added to exactly ONE element. 
+// The EventListener function will have to include two for-loops: one to loop through and find the element that is most "near the top"--the one with the smallest nonzero y.
+// the secton loop will then have to apply the... Wait, since the elements will be in ascending-y order, the loop just needs to look for the first non-zero y, and then remove "active" from all the others.
+window.addEventListener('scroll', function () {
+    for (section of sectionsArray) {
+        if elementIsNearTop(section) {
+            section.classList.add('active-section');
+        } else {
+            section.classList.remove('active-section');        
+        }
+    }
+}
+
+
 
 
 // Scroll to anchor ID using scrollTO event
